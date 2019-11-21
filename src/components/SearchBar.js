8888b1569f0b8615eeb8008dc5c05e3d8adfd32f
Mike <mike.castellon5@gmail.com>
2019-11-21 16:41:25 -0500

    added search bar styling and basic functionality

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import  { View, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+const SearchBar = ({ term, onTermChange }) => {
+  return(
+    <View style= {styles.backgroundStyle}>
+      <FontAwesome style={styles.iconStyle} name='search' />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect= {false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value= {term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+})
+
+export default SearchBar;

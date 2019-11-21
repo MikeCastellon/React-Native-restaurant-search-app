288a223794b902a89cfd4dcfb63b1428e0b78bc6
Mike <mike.castellon5@gmail.com>
2019-11-21 16:52:22 -0500

    added axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,9 +6,13 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('')
   return(
     <View>
-      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={ () => console.log('term submitted')}
+      />
       <Text> Search Screen</Text>
-  
+
     </View>
   )
 }

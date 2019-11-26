0c67c3816c9d7191edc45a522c6b2680b38bdc53
Mike <mike.castellon5@gmail.com>
2019-11-26 15:25:50 -0500

    added useEffect and made my own hook for search results

---
+++ src/components/ResultsList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const ResultsList = ({ title }) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+})
+
+
+export default ResultsList;
